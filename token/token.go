0a294ada1bf05d132f78f12e4805c36bfd27125a
林庭楷 <hellolinpeter@gmail.com>
2024-02-14 20:15:07 +0800

    詞法分析

--- token/token.go
+++ token/token.go
@@ -2,25 +2,50 @@ package token
 
 type TokenType string
 
-type Token struct{
-	Type TokenType
+type Token struct {
+	Type    TokenType
 	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
 
 const (
 	ILLEGAL = "ILLEGAL" //遇到未知詞法或字符
-	EOF = "EOF" //文件結尾
+	EOF     = "EOF"     //文件結尾
 
 	//標示符＋字面量
 	IDENT = "IDENT" //add,foobar,x,y...
-	INT = "INT" //123456
+	INT   = "INT"   //123456
 
 	//運算符
-	ASSIGN = "="
-	PLUS = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOTEQ = "!="
 	//分隔符
-	COMMA = ","
+	COMMA     = ","
 	SEMICOLON = ";"
 
 	LPAREN = "("
@@ -30,5 +55,10 @@ const (
 
 	//關鍵字
 	FUNCTION = "FUNCTION"
-	LET = "LET"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
