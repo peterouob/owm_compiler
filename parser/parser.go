4d48474e6f8724e1d3e4129386a9c9f176d90721
林庭楷 <hellolinpeter@gmail.com>
2024-03-03 14:53:25 +0800

    trace

--- parser/parser.go
+++ parser/parser.go
@@ -153,6 +153,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 }
 
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	defer untrace(trace("parseExpressionStatement"))
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
 	stmt.Expression = p.parseExpression(LOWSET)
 
@@ -164,6 +165,7 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 
 // 普拉斯解釋器核心
 func (p *Parser) parseExpression(precedence int) ast.Expression {
+	defer untrace(trace("parseExpression"))
 	prefix := p.prefixParseFn[p.curToken.Type]
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
@@ -217,6 +219,8 @@ func (p *Parser) parseIdentifier() ast.Expression {
 }
 
 func (p *Parser) parseIntegerLiteral() ast.Expression {
+	defer untrace(trace("parseIntegerLiteral"))
+
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
 	if err != nil {
@@ -229,6 +233,8 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
+	defer untrace(trace("parsePrefixExpression"))
+
 	expression := &ast.PrefixExpression{
 		Token:    p.curToken,
 		Operator: p.curToken.Literal,
@@ -240,6 +246,8 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 }
 
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	defer untrace(trace("parseInfixExpression"))
+
 	expression := &ast.InfixExpression{
 		Token:    p.curToken,
 		Operator: p.curToken.Literal,
---
+++ parser/parser_tracing.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"fmt"
+	"strings"
+)
+
+var tracingLevel int = 0
+
+const traceIdentPlaceholder string = "\t"
+
+func identLevel() string {
+	return strings.Repeat(traceIdentPlaceholder, tracingLevel-1)
+}
+
+func tracePrint(fs string) {
+	fmt.Printf("%s%s\n", identLevel(), fs)
+}
+func incIdentLevel() { tracingLevel = tracingLevel + 1 }
+func decIdentLevel() { tracingLevel = tracingLevel - 1 }
+
+func trace(msg string) string {
+	incIdentLevel()
+	tracePrint("BEGIN" + msg)
+	return msg
+}
+
+func untrace(msg string) {
+	tracePrint("END" + msg)
+	decIdentLevel()
+}
