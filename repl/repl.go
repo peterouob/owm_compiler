cc84bfa2bcd68bf1c0407654076d1371051029cf
林庭楷 <hellolinpeter@gmail.com>
2024-03-21 21:57:16 +0800

    repl and show on terminal

--- repl/repl.go
+++ repl/repl.go
@@ -3,7 +3,7 @@ package repl
 import (
 	"bufio"
 	"compiler/lexer"
-	"compiler/token"
+	"compiler/parser"
 	"fmt"
 	"io"
 )
@@ -21,9 +21,19 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParserProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
